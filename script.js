19d67954239a8c6e5165531b28eff4b03fedb027
Aayush Rai <coolaayushrai@gmail.com>
2024-04-26 13:37:47 +0530

    Turn all required and empty inputs inaccessible to :invalid pseudo-class
    Initially, all the inputs that were empty and had the 'required'
    attribute set on them were being targeted by :invalid pseudo-class.
    This led to the :invalid styling being forcefully applied on all
    empty inputs.
    
    Therefore a class of 'empty' is applied to all empty inputs and :invalid
    styling is done to only those inputs that do not have that class (which
    is toggled with the help of event listener.)

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//Invalid input element styling
+const inputList = document.querySelectorAll('input');
+for (let i=0; i<inputList.length; i++)
+{
+    inputList[i].addEventListener('input', () => {
+        if (inputList[i].value == '')
+        {
+            inputList[i].setAttribute('class', 'empty');
+        }
+        else
+        {
+            inputList[i].removeAttribute('class', 'empty');
+        }
+    });
+}
+
+//Password-match checking algorithm
 const confirmPass = document.querySelector('#confirm-pass');
 const pass = document.querySelector('#pass');
 
